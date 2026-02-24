585554820e61c1d7d7f7441a2296bc507088702f
Mohan R <r7811158_bcs27@mepcoeng.ac.in>
2026-02-24 16:34:21 +0000

    Sync LeetCode submission Runtime - 4 ms (28.57%), Memory - 19.5 MB (42.21%)

--- solutions/0004-median-of-two-sorted-arrays/solution.py
+++ solutions/0004-median-of-two-sorted-arrays/solution.py
@@ -1,13 +1,10 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        # Merge the two sorted arrays
-        merged = sorted(nums1 + nums2)
-        length = len(merged)
-        
-        # Check if the total length is even or odd
-        if length % 2 == 0:
-            # If even, return the average of the two middle elements
-            return (merged[length // 2 - 1] + merged[length // 2]) / 2
+        ar3=nums1+nums2
+        n=len(ar3)
+        ar3.sort()
+        mid= n//2
+        if n%2==0:
+            return float(ar3[mid]+ar3[mid-1])/2
         else:
-            # If odd, return the middle element
-            return merged[length // 2]
+            return float(ar3[mid])
