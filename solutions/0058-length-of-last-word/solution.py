e97e4cbe82a6343a444cfcf4aa15a03eecf42e40
Mohan R <r7811158_bcs27@mepcoeng.ac.in>
2026-03-30 10:21:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (53.04%)

---
+++ solutions/0058-length-of-last-word/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        # split() without arguments splits by any whitespace and removes empty strings
+        words = s.split()
+        return len(words[-1]) if words else 0
+
