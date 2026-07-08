56c66d813fac829eded6d0ea4382744928893d3c
Mohan R <r7811158_bcs27@mepcoeng.ac.in>
2026-07-08 10:27:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (19.24%)

--- solutions/0027-remove-element/solution.py
+++ solutions/0027-remove-element/solution.py
@@ -1,15 +1,13 @@
 class Solution:
-    def removeElement(self, nums: list[int], val: int) -> int:
-        # k will be the index where we place the next element != val
-        k = 0
-        
+    def removeElement(self, nums: List[int], val: int) -> int:
+        count = 0
+        ar =[]
         for i in range(len(nums)):
-            # If the current element is not the target value
-            if nums[i] != val:
-                # Move it to the 'k' position and increment k
-                nums[k] = nums[i]
-                k += 1
-                
-        # k is the count of elements not equal to val
-        return k
-
+            if nums[i]==val:
+                count = count +1
+            else:
+                ar.append(nums[i])
+        for i in range(len(ar)):
+            nums[i] = ar[i]
+            
+        return len(nums) - count
