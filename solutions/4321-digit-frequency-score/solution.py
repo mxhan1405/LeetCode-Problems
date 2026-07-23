8874253a829bf1718bcd4bcc59b66c2c052b7777
Mohan R <r7811158_bcs27@mepcoeng.ac.in>
2026-07-23 10:20:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (88.23%)

--- solutions/4321-digit-frequency-score/solution.py
+++ solutions/4321-digit-frequency-score/solution.py
@@ -1,6 +1,17 @@
 class Solution:
     def digitFrequencyScore(self, n: int) -> int:
-        sum=0
-        for k in str(n):
-            sum+=int(k)
-        return sum
+        d={}
+        while n>0:
+            t=n%10
+            if t not in d.keys():
+                d[t]=1
+            else:
+                d[t]+=1
+            n=n//10
+        ans=0
+        for k in d.keys():
+            ans+=(k*d[k])
+        return ans
+
+
+
